28970e3e0409136a2b251711e49ffce9a8508d10
Jimmy Jam <jimmymano@mail.com>
2021-07-20 01:08:56 -0500

    Created Sign In/Register

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React, {Component} from 'react';
 import Navigation from './components/Navigation/Navigation';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
@@ -30,7 +32,9 @@ class App extends React.Component{
     this.state={
       input:'',
       imageUrl:'',
-      box:{}
+      box:{},
+      route:'signin',
+      isSignedIn:'false'
     }
   }
 
@@ -68,17 +72,35 @@ class App extends React.Component{
     
   }
 
+  onRouteChange = (route) =>{
+    if(route === 'signout'){
+      this.setState({isSignedIn:false})
+    }else if(route ==='home'){
+      this.setState({isSignedIn:true})
+    }
+    this.setState({route:route}) 
+  }
+
   render(){
   return (
     <div className="App">
       <Particles className='particles'params={particlesOptions}/>
-      <Navigation/>
+      <Navigation onRouteChange={this.onRouteChange}/>
+      {this.state.route==='home'
+      ?<div>
       <Logo/>
       <Rank/>
       <ImageLinkForm 
       onInputChange={this.onInputChange} 
       onButtonSubmit={this.onButtonSubmit}/> 
       <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+      </div>
+      :(
+        this.state.route==='signin'
+      ?<SignIn onRouteChange={this.onRouteChange}/>
+      :<Register onRouteChange={this.onRouteChange}/>
+      )
+      }
     </div>
   );
 }
