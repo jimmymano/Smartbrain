01ef48d05de2ec6c14f73fb5ada1a9aeeca403bd
Jimmy Jam <jimmymano@mail.com>
2021-09-06 19:39:14 -0500

    updated security

--- src/App.js
+++ src/App.js
@@ -9,11 +9,7 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import Particles from 'react-particles-js';
 import 'tachyons';
-import Clarifai from 'clarifai';
-
-const app = new Clarifai.App({
- apiKey: 'ec514acc370b4b3caa8fe2e9b1790573'
-});
+// import Clarifai from 'clarifai';
 
 const particlesOptions = {
   particles: {number:{
@@ -88,10 +84,14 @@ componentDidMount(){
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    app.models
-      .predict(
-      Clarifai.FACE_DETECT_MODEL,
-        this.state.input)
+    fetch('http://localhost:3000/imageurl', {
+      method: 'put',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        input: this.state.input
+      })
+    })
+    .then(response => response.json())
       .then(response => {
         if (response) {
           fetch('http://localhost:3000/image', {
